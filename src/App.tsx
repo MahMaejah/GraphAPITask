ba081d3aa9f6b7fcc376e657e9b01235f8f61a4d
MahMaejah <chipohameja@gmail.com>
2022-03-08 10:46:31 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { MsalProvider } from '@azure/msal-react'
+import { IPublicClientApplication } from '@azure/msal-browser';
+
+import ProvideAppContext from './AppContext';
+import ErrorMessage from './ErrorMessage';
+import NavBar from './NavBar';
+import Welcome from './Welcome';
+import 'bootstrap/dist/css/bootstrap.css';
+import Calendar from './Calendar';
+import NewEvent from './NewEvent';
+
+type AppProps= {
+  pca: IPublicClientApplication
+};
+
+export default function App({ pca }: AppProps) {
+  return(
+    <MsalProvider instance={ pca }>
+      <ProvideAppContext>
+        <Router>
+          <div>
+            <NavBar />
+            <Container>
+              <ErrorMessage />
+              <Route exact path="/"
+                render={(props) =>
+                  <Welcome {...props} />
+                } />
+
+                <Route exact path="/calendar"
+                render={(props) =>
+                  <Calendar {...props} />
+                } />
+
+              <Route exact path="/newevent"
+                render={(props) =>
+                  <NewEvent {...props} /> 
+                } />
+            </Container>
+          </div>
+        </Router>
+      </ProvideAppContext>
+    </MsalProvider>
+  );
+}
---
+++ src/Config.ts
@@ -0,0 +1,11 @@
+const config = {
+    appId: 'ec44b738-c7c0-4b53-838b-a3b1a65fc2e3',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+      'user.read',
+      'mailboxsettings.read',
+      'calendars.readwrite'
+    ]
+  };
+  
+  export default config;
